861aebdfb66fb5e3ecf23ebb2056d68f4f085fdc
Developer <developer@localhost.localdomain>
2019-04-22 17:58:10 -0500

    Moving into database query chapter, back to adding PHP functionality

---
+++ course_files/db_connect_guide.php
@@ -0,0 +1,28 @@
+<?php
+
+// This guide demonstrates the five fundamental steps
+// of database interaction using PHP.
+
+// Credentials
+$dbhost = 'localhost';
+$dbuser = 'webuser';
+$dbuser = 'secretpassword';
+$dbname = 'globe_bank';
+
+// 1. Create a database connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// 2. Perform database query
+$query = "SELECT * FROM subjects;";
+mysqli_query($connection, $query);
+
+// 3. Use returned data (if any)
+
+// 4. Release returned data
+mysqli_free_result($result_set);
+
+// 5. Close database connection
+mysqli_close($connection);
+
+?>
+ 
