3addf0e93b158f99942bd4b199aad4c89ca70314
Homer Simpson <gomez@persephone.gomezengineering.lan>
2019-04-27 17:17:44 -0500

    Viewed videos & made most code changes to complete Chapter 6 (database usage). Have NOT set up mysql completely, or created and applied the SQL to populate the database.

--- globe_bank/private/db_credentials.php
+++ globe_bank/private/db_credentials.php
@@ -3,7 +3,7 @@
 
 define ("DB_SERVER", "localhost");
 define ("DB_USER", "webuser");
-define ("DB_PASS", "secretpassword");
+define ("DB_PASS", "secretpasswordX");
 define ("DB_NAME", "globe_bank");
 
 ?>
