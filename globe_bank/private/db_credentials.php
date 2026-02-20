0dc1540c038b361c748641e376bd22acb8210389
Steven Gomez <steve_gomez@usa.net>
2026-02-19 22:08:09 -0600

    Committing historical changes

--- globe_bank/private/db_credentials.php
+++ globe_bank/private/db_credentials.php
@@ -3,7 +3,7 @@
 
 define("DB_SERVER", "localhost");
 define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
+define("DB_PASS", "SecretPassw0rd");
 define("DB_NAME", "globe_bank");
 
 ?>
